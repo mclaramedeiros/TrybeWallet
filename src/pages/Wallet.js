93e2069882b7527bf5566a3386ea4ec86ad8f106
Maria Clara <claramedeiroswon@gmail.com>
2022-04-01 16:32:38 -0300

    adiciona prop validation

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class Wallet extends React.Component {
@@ -19,5 +20,9 @@ class Wallet extends React.Component {
 const mapStateToProps = (state) => ({
   email: state.user.email,
 });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps)(Wallet);
