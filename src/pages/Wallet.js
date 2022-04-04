9a68e7ad4cb23216791c4253b7b1ec53cc397853
Maria Clara <claramedeiroswon@gmail.com>
2022-04-04 16:23:55 -0300

    requisito 8

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -63,7 +63,10 @@ class Wallet extends React.Component {
   }
 
   render() {
-    const { email, fetchCurrencies } = this.props;
+    const { email, fetchCurrencies, keyValue } = this.props;
+    const allTheCategories = ['Descrição', 'Tag', 'Método de pagamento',
+    'Valor', 'Moeda', 'Câmbio utilizado',
+    'Valor convertido', 'Moeda de conversão', 'Editar/Excluir'];
     const { value, description, currency, method, tag } = this.state;
     return (
       <div>
@@ -85,6 +88,64 @@ class Wallet extends React.Component {
           tag={ tag }
 
         />
+        <table>
+          <thead>
+            <tr>
+              {allTheCategories.map((element) => (
+                <th key={ element }>{element}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {keyValue.map((element) => (
+              <tr key={ element.id }>
+                <td key={ element.id }>
+                  { element.description }
+                </td>
+                <td key={ element.id }>
+                  { element.tag }
+                </td>
+                <td key={ element.id }>
+                  { element.method }
+                </td>
+                <td key={ element.id }>
+                  { Number(element.value).toFixed(2) }
+                </td>
+                <td key={ element.id }>
+                  { (element.exchangeRates[element.currency].name).split('/', 1) }
+                </td>
+                <td key={ element.id }>
+                  {Number(element.exchangeRates[element.currency].ask).toFixed(2) }
+                </td>
+                <td key={ element.id }>
+                  {(Number(element.value)
+                * Number(element.exchangeRates[element.currency].ask)).toFixed(2) }
+                </td>
+                <td key={ element.id }>
+                  Real
+                </td>
+                <td key={ element.id }>
+                  Editar/Exclui
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {/* <table>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table> */}
       </div>
     );
   }
